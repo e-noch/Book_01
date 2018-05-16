51d12c322dd3ee58fd608f4014873c8ea18c3605
Enoch <enochda@aol.com>
2018-05-15 21:33:24 -0400

    HTML, CSS, JS
    Adding slideshow

--- js/nav_animate.js
+++ js/nav_animate.js
@@ -10,7 +10,7 @@
 //jQuery code
 $(document).ready (function(){
 	//add to hide header
-	$('header').hide().slideDown(2000);
+	$('header').hide().fadeIn(2000);
 	//icon animation
 	$('.icon').click(function(){
 		$(this)
---
+++ js/slideshow.js
@@ -0,0 +1,43 @@
+// JavaScript Document
+/*
+// Demo Slid Show JS
+// Enoch Daniel Adams
+// For Demo
+// May 11, 2018
+*/
+window.onload =inintlinks;
+// js alwyas start with var
+//gobal array
+var myImages = new Array ("./images/vac1.jpg",
+						 "./images/vac2.jpg",
+						 "./images/vac3.jpg",
+						 "./images/vac4.jpg",
+						 "./images/vac5.jpg",
+						 "./images/piz1.jpg",
+						 "./images/piz2.jpg",
+						 "./images/piz3.jpg");
+var thisPic=0;//start here
+function inintlinks(){
+	'use strict';
+	//alert("Nioce");
+	document.getElementById("prevLink").onclick=processPrevious; //look for function
+	document.getElementById("nextLink").onclick=processNext;
+}//initLinks
+function processPrevious(){
+	'use strict';
+	if (thisPic === 0){ // the image
+		thisPic=myImages.length; // the array length
+	}//if
+	thisPic--; // deduct from the length 
+	document.getElementById("myPicture").src=myImages[thisPic]; // display pricture
+	return false;
+}//processPrevious
+function processNext(){
+		'use strict';
+	thisPic++; // incress one to length or add one to current image
+	if (thisPic === myImages.length){ // reset value
+		thisPic=0;
+	}//if
+	document.getElementById("myPicture").src=myImages[thisPic];// replaces image with new image
+	return false;
+}//processNext
