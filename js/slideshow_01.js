44602491cfa3a9d34e90b6329e251c39f7903a86
Enoch <enochda@aol.com>
2018-06-04 21:34:38 -0400

    css, js
    slider update

---
+++ js/slideshow_01.js
@@ -0,0 +1,44 @@
+// JavaScript Document
+/*
+//
+// Malacandra Slid Show JS
+// Enoch Daniel Adams
+// @dustanashes
+// June 04, 2018
+//
+*/
+window.onload =inintlinks;
+// js alwyas start with var
+//gobal array
+var myImages = new Array (	
+						 "./images/IMG_2155.jpg",
+						 "./images/IMG_2161.JPG",
+						 "./images/IMG_2162.JPG",
+						 "./images/IMG_2177.JPG",
+						 "./images/IMG_2179.JPG",
+						 "./images/IMG_2185.JPG");
+var thisPic=0;//start here
+function inintlinks(){
+	'use strict';
+	//alert("Nioce");
+	document.getElementById("prevLink").onclick=processPrevious; //look for function
+	document.getElementById("nextLink").onclick=processNext;
+}//initLinks
+function processPrevious(){
+	'use strict';
+	if (thisPic === 0){ // the image
+		thisPic=myImages.length; // the array length
+	}//if
+	thisPic--; // deduct from the length 
+	document.getElementById("myPicture").src=myImages[thisPic]; // display pricture
+	return false;
+}//processPrevious
+function processNext(){
+		'use strict';
+	thisPic++; // incress one to length or add one to current image
+	if (thisPic === myImages.length){ // reset value
+		thisPic=0;
+	}//if
+	document.getElementById("myPicture").src=myImages[thisPic];// replaces image with new image
+	return false;
+}//processNext
