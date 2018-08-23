2b43b525929bb610a461ec023bf54ac0058e7edb
Enoch <enochda@aol.com>
2018-08-23 19:16:07 -0400

    Content Update
    index dot html update

---
+++ js/slickInitialization.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+
+$(document).ready (function(){
+	//alert("Nioce");
+	$('.bSlider').slick({
+	autoplay: true,
+	autoplaySpeed: 3000,
+	infinite: true,
+  	speed: 500,
+  	fade: true,
+  	cssEase: 'linear'
+	});
+});//end ready
